f6aa86650cac32db9446440a503e38a9317282fb
Zjl37 <2693911885@qq.com>
2023-07-23 11:36:51 +0800

    将ids解析从别的项目中分离出来

---
+++ src/io.rs
@@ -0,0 +1,138 @@
+use once_cell::sync::Lazy;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fs;
+use std::io::{self, BufRead};
+
+thread_local!(
+    static IDS_FILE_LIST: RefCell<Vec<String>> = RefCell::new(vec!["./ids/ids_lv0.txt".to_string()]);
+);
+
+pub fn set_ids_file_list(v: Vec<String>) {
+    IDS_FILE_LIST.with(|fl| {
+        *fl.borrow_mut() = v;
+    });
+}
+
+pub fn add_ids_file(f: String) {
+    IDS_FILE_LIST.with(|fl| {
+        fl.borrow_mut().push(f);
+    });
+}
+
+fn ids_split_var(s: &str) -> Box<dyn Iterator<Item = (&str, &str)> + '_> {
+    if s.ends_with(')') {
+        if let Some(p) = s.rfind('(') {
+            if s.get(p - 1..p) != Some("#") {
+                return Box::new(
+                    s[p + 1..s.len() - 1]
+                        .split(',')
+                        .map(move |var| (var, &s[0..p])),
+                );
+            }
+        } else {
+            eprintln!("ids 语法可能有误：{}", s);
+        }
+    }
+    return Box::new(vec![("", s)].into_iter());
+}
+
+pub fn load() -> Result<HashMap<char, HashMap<String, String>>, io::Error> {
+    let mut ids_map = HashMap::new();
+
+    let mut process_1 = |f: io::BufReader<_>| {
+        for ln in f.lines() {
+            if let Ok(ln) = ln {
+                if ln.starts_with("*") {
+                    continue;
+                }
+
+                let v: Vec<&str> = ln.split('\t').collect();
+                if v.len() < 2 {
+                    continue;
+                }
+                let mut it = v[0].chars();
+                let ch = it.next().unwrap();
+                if it.next().is_some() {
+                    eprintln!("ids 文件有误：'{}'不是一个字", v[0]);
+                    continue;
+                }
+
+                let hm = ids_map.entry(ch).or_insert(HashMap::new());
+                for seq in v[1].split(';').map(ids_split_var) {
+                    for (var, ids) in seq {
+                        hm.insert(var.to_string(), ids.to_string());
+                    }
+                }
+            }
+        }
+    };
+
+    IDS_FILE_LIST.with(|fl| -> Result<(), io::Error> {
+        for filename in fl.borrow().iter() {
+            let file = fs::File::open(filename)?;
+            process_1(io::BufReader::new(file));
+        };
+        Ok(())
+    })?;
+
+    eprintln!("成功加载全部 ids 数据，一共有 {} 条记录", ids_map.len());
+
+    Ok(ids_map)
+}
+
+static IDS_MAP: Lazy<HashMap<char, HashMap<String, String>>> = Lazy::new(|| load().unwrap());
+
+pub fn query(ch: char, variant: &Vec<&str>) -> Option<(&'static str, &'static str)> {
+    if let Some(vars_avail) = IDS_MAP.get(&ch) {
+        let tuple2str = |p: (&'static String, &'static String)| (p.0.as_str(), p.1.as_str());
+        let get_1 = || vars_avail.iter().next().map(tuple2str);
+
+        if vars_avail.len() <= 1 {
+            return get_1();
+        }
+
+        for &var in variant {
+            if let Some((v, s)) = vars_avail.get_key_value(var) {
+                return Some((v.as_str(), s.as_str()));
+            }
+        }
+
+        let ret = vars_avail.get_key_value("").map(tuple2str).or_else(get_1);
+        if let Some((v, _)) = ret {
+            eprintln!(
+                "找不到'{}'字的({})字形，将使用({})字形。",
+                ch,
+                variant.join(","),
+                v
+            );
+        }
+        return ret;
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use super::*;
+
+    #[test]
+    fn all_tree() -> Result<(), Box<dyn std::error::Error>> {
+        use crate::ids;
+
+        for (i, (ch, hm)) in IDS_MAP.iter().enumerate() {
+            eprintln!("===== {:6} / {:6} ===== ", i + 1, IDS_MAP.len());
+            let _ = io::stdout().flush();
+            for (var, s) in hm {
+                println!("==> {}", s);
+                println!("{}", ids::create_tree(*ch, vec![var.as_str()])?);
+                println!();
+            }
+            println!()
+        }
+
+        Ok(())
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,261 @@
+use pest::iterators::Pair;
+use pest::Parser;
+
+pub mod io;
+
+#[derive(pest_derive::Parser)]
+#[grammar = "ids.pest"]
+pub struct IDSParser;
+
+pub enum IDSExpr {
+    Null,
+    UnaryExpr {
+        op: char,
+        arg1: Box<IDSNode>,
+    },
+    BinExpr {
+        op: char,
+        arg1: Box<IDSNode>,
+        arg2: Box<IDSNode>,
+        op_arg: Option<String>,
+    },
+    TerExpr {
+        op: char,
+        arg1: Box<IDSNode>,
+        arg2: Box<IDSNode>,
+        arg3: Box<IDSNode>,
+    },
+}
+
+pub struct IDSNode {
+    pub ideographic: Option<char>,
+    pub stroke_seq: Option<String>,
+    pub glyph_hint: String,
+    pub subtree: IDSExpr,
+    pub glyph_variant: String,
+}
+
+pub struct IDS {
+    pub root: IDSNode,
+}
+
+impl IDSNode {
+    fn fmt_r(&self, f: &mut std::fmt::Formatter<'_>, level: usize) -> std::fmt::Result {
+        write!(f, "{:width$}* ", "", width = level * 4)?;
+        match &self.subtree {
+            IDSExpr::UnaryExpr { op, .. } => {
+                write!(f, "{}", op)?;
+            }
+            IDSExpr::BinExpr {
+                op,
+                op_arg: overlay_arg,
+                ..
+            } => {
+                write!(f, "{}", op)?;
+                if let Some(s) = overlay_arg {
+                    write!(f, "[{}]", s)?;
+                }
+            }
+            IDSExpr::TerExpr { op, .. } => write!(f, "{}", op)?,
+            _ => (),
+        }
+        write!(f, "\t")?;
+        if let Some(ch) = self.ideographic {
+            write!(f, "{}", ch)?;
+            if self.glyph_variant.len() != 0 {
+                write!(f, "({})", self.glyph_variant)?;
+            }
+            write!(f, " ")?
+        }
+        if let Some(s) = &self.stroke_seq {
+            write!(f, "{} ", s)?;
+        }
+        if self.glyph_hint.len() > 0 {
+            write!(f, "Hint: {{{}}}", self.glyph_hint)?;
+        }
+        writeln!(f)?;
+
+        match &self.subtree {
+            IDSExpr::UnaryExpr { arg1, .. } => arg1.fmt_r(f, level + 1),
+            IDSExpr::BinExpr { arg1, arg2, .. } => {
+                arg1.fmt_r(f, level + 1)?;
+                arg2.fmt_r(f, level + 1)
+            }
+            IDSExpr::TerExpr {
+                op: _,
+                arg1,
+                arg2,
+                arg3,
+            } => {
+                arg1.fmt_r(f, level + 1)?;
+                arg2.fmt_r(f, level + 1)?;
+                arg3.fmt_r(f, level + 1)
+            }
+            _ => Ok(()),
+        }
+    }
+}
+
+impl std::fmt::Display for IDS {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(ch) = self.root.ideographic {
+            write!(f, "'{}' ", ch)?;
+        }
+        writeln!(f)?;
+        self.root.fmt_r(f, 0)
+    }
+}
+
+fn parse_node_fr(pair: Pair<Rule>) -> IDSNode {
+    match pair.as_rule() {
+        Rule::ideographicA => IDSNode {
+            ideographic: Some(pair.as_str().chars().next().unwrap()),
+            stroke_seq: None,
+            glyph_hint: "".to_string(),
+            subtree: IDSExpr::Null,
+            glyph_variant: match pair.into_inner().skip(1).next() {
+                Some(p) => p.as_str().to_string(),
+                None => "".to_string(),
+            },
+        },
+        Rule::strokeExpr => {
+            let mut inner_pairs = pair.into_inner();
+            let mut hint = "";
+            if inner_pairs.peek().unwrap().as_rule() == Rule::hint {
+                hint = inner_pairs.next().unwrap().as_str();
+                hint = &hint[1..hint.len() - 1];
+            }
+            IDSNode {
+                ideographic: None,
+                stroke_seq: Some(inner_pairs.next().unwrap().as_str().to_string()),
+                glyph_hint: hint.to_string(),
+                subtree: IDSExpr::Null,
+                glyph_variant: "".to_string(),
+            }
+        }
+        Rule::unaryExpr => {
+            let mut inner_pairs = pair.into_inner();
+
+            IDSNode {
+                ideographic: None,
+                stroke_seq: None,
+                glyph_hint: "".to_string(),
+                subtree: IDSExpr::UnaryExpr {
+                    op: inner_pairs.next().unwrap().as_str().chars().next().unwrap(),
+                    arg1: Box::new(parse_node_fr(inner_pairs.next().unwrap())),
+                },
+                glyph_variant: "".to_string(),
+            }
+        }
+        Rule::binExpr => {
+            let mut inner_pairs = pair.into_inner();
+            let mut hint = "";
+            if inner_pairs.peek().unwrap().as_rule() == Rule::hint {
+                hint = inner_pairs.next().unwrap().as_str();
+                hint = &hint[1..hint.len() - 1];
+            }
+            let pair_op = inner_pairs.next().unwrap();
+            let ch = pair_op.as_str().chars().next().unwrap();
+            let op_arg = pair_op
+                .into_inner()
+                .next()
+                .map(|p| p.as_str())
+                .map(|s| s[1..s.len() - 1].to_string());
+
+            IDSNode {
+                ideographic: None,
+                stroke_seq: None,
+                glyph_hint: hint.to_string(),
+                subtree: IDSExpr::BinExpr {
+                    op: ch,
+                    arg1: Box::new(parse_node_fr(inner_pairs.next().unwrap())),
+                    arg2: Box::new(parse_node_fr(inner_pairs.next().unwrap())),
+                    op_arg,
+                },
+                glyph_variant: "".to_string(),
+            }
+        }
+        Rule::terExpr => {
+            let mut inner_pairs = pair.into_inner();
+            let mut hint = "";
+            if inner_pairs.peek().unwrap().as_rule() == Rule::hint {
+                hint = inner_pairs.next().unwrap().as_str();
+                hint = &hint[1..hint.len() - 1];
+            }
+
+            IDSNode {
+                ideographic: None,
+                stroke_seq: None,
+                glyph_hint: hint.to_string(),
+                subtree: IDSExpr::TerExpr {
+                    op: inner_pairs.next().unwrap().as_str().chars().next().unwrap(),
+                    arg1: Box::new(parse_node_fr(inner_pairs.next().unwrap())),
+                    arg2: Box::new(parse_node_fr(inner_pairs.next().unwrap())),
+                    arg3: Box::new(parse_node_fr(inner_pairs.next().unwrap())),
+                },
+                glyph_variant: "".to_string(),
+            }
+        }
+        Rule::expr => parse_node_fr(pair.into_inner().next().unwrap()),
+        _ => unreachable!(),
+    }
+}
+
+fn extend_var_list<'a>(l: &Vec<&'a str>, v: &'a str) -> Vec<&'a str> {
+    let mut ret = vec![v];
+    ret.extend(l.iter().filter(|&&s| s != v));
+    return ret;
+}
+
+pub fn extend_node(i: &mut IDSNode, var: &Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    let check_extend = |j: &mut Box<IDSNode>| -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(ch) = j.ideographic {
+            if let IDSExpr::Null = j.subtree {
+                let node = create_node(ch, extend_var_list(var, &j.glyph_variant))?;
+                *j = Box::new(node);
+            }
+        }
+        Ok(())
+    };
+    match &mut i.subtree {
+        IDSExpr::BinExpr {
+            ref mut arg1,
+            ref mut arg2,
+            ..
+        } => {
+            check_extend(arg1)?;
+            check_extend(arg2)
+        }
+        IDSExpr::TerExpr {
+            op: _,
+            ref mut arg1,
+            ref mut arg2,
+            ref mut arg3,
+        } => {
+            check_extend(arg1)?;
+            check_extend(arg2)?;
+            check_extend(arg3)
+        }
+        _ => Ok(()),
+    }
+}
+
+pub fn create_node<'a>(ch: char, var: Vec<&str>) -> Result<IDSNode, Box<dyn std::error::Error>> {
+    if let Some((v, s)) = io::query(ch, &var) {
+        let ids0 = IDSParser::parse(Rule::expr, s)?.next().unwrap();
+        let mut root = parse_node_fr(ids0);
+
+        root.ideographic = Some(ch);
+        root.glyph_variant = v.to_string();
+        extend_node(&mut root, &var)?;
+        return Ok(root);
+    }
+
+    Err("在 create_node 中 查询 IDS 失败".into())
+}
+
+pub fn create_tree(ch: char, var: Vec<&str>) -> Result<IDS, Box<dyn std::error::Error>> {
+    Ok(IDS {
+        root: create_node(ch, var)?,
+    })
+}
